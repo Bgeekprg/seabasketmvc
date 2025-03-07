e483ba335a02433c3b1a58b8073308dbd6c3fba1
Bgeekprg <bhaveshpr54@gmail.com>
2025-03-07 10:38:42 +0530

    [feature] Boilerplate setup done

---
+++ helper/logger_helper.py
@@ -0,0 +1,19 @@
+import logging
+import os
+
+logging_levels = {
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "error": logging.ERROR,
+    "warning": logging.WARNING,
+    "critical": logging.CRITICAL,
+}
+
+
+def setup_logger():
+    log_level = (
+        logging.DEBUG
+        if os.getenv("LOG_LEVEL") == None
+        else logging_levels[os.getenv("LOG_LEVEL")]
+    )
+    logging.basicConfig(level=log_level)

82357be82cb2719245d8e0ec954dbf33b7a2d684
Bgeekprg <bhaveshpr54@gmail.com>
2025-03-07 18:26:53 +0530

    add: added sessionmaker for orm of sqlAlchemy

---
+++ config/db_config.py
@@ -0,0 +1,18 @@
+# Importing libraries
+from sqlalchemy import MetaData
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import sessionmaker
+import os
+
+# Initialization
+meta = MetaData()
+
+
+# Creating engine
+auth = f"{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASSWORD')}"
+engine = create_engine(
+    f"mysql+mysqlconnector://{auth}@{os.getenv('DATABASE_URL')}:{os.getenv('DATABASE_PORT')}/{os.getenv('DATABASE_NAME')}",
+    pool_recycle=3600,
+)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)

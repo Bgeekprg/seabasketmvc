3f2a8fe6d177781b438dadc9fc6f173847fd1396
Bgeekprg <bhaveshpr54@gmail.com>
2025-03-07 18:21:27 +0530

    feat: added db helper for authentication

---
+++ utils/db_helper.py
@@ -0,0 +1,20 @@
+from fastapi import Depends
+from sqlalchemy import select
+from config.db_config import SessionLocal, engine
+from models.users_table import User
+from sqlalchemy.orm import Session
+
+
+class DBHelper:
+
+    def get_user_by_email(email: str):
+        with SessionLocal() as db:
+            return db.query(User).filter(User.email == email).first()
+
+    def get_user_by_phone_number(phone_number: str):
+        with SessionLocal() as db:
+            return db.query(User).filter(User.phoneNumber == phone_number).first()
+
+    def get_user_by_id(id: int):
+        with SessionLocal() as db:
+            return db.query(User).filter(User.id == id).first()

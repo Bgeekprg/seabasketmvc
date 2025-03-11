bfa6f58f25b8cf79bb54ac315010b0508267bd9d
Bgeekprg <bhaveshpr54@gmail.com>
2025-03-11 12:52:48 +0530

    feat: added products SqlAlchemy model

---
+++ models/products_table.py
@@ -0,0 +1,28 @@
+from sqlalchemy import TEXT, TIMESTAMP, Enum, String, DECIMAL, Column, func, ForeignKey
+from sqlalchemy.sql.sqltypes import DateTime, Integer, Text, Boolean
+from config.db_config import meta
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from config.db_config import Base
+from models.categories_table import Category
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    description = Column(TEXT, nullable=True)
+    stockQuantity = Column(Integer, default=0)
+    price = Column(DECIMAL(10, 2), nullable=False)
+    categoryId = Column(Integer, ForeignKey("categories.id"), nullable=True)
+    productUrl = Column(String(255), nullable=True)
+    discount = Column(Integer, nullable=True)
+    rating = Column(DECIMAL(3, 2), nullable=True)
+    isAvailable = Column(Boolean, default=True)
+    createdAt = Column(TIMESTAMP, default=func.current_timestamp())
+    updatedAt = Column(
+        TIMESTAMP, default=func.current_timestamp(), onupdate=func.current_timestamp()
+    )
+
+    category = relationship("Category", back_populates="products")

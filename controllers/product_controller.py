3b4f70bfdf0efda1331cc7c15aed2aa88e8f007b
Bgeekprg <bhaveshpr54@gmail.com>
2025-03-11 12:52:09 +0530

    feat: added product controller

---
+++ controllers/product_controller.py
@@ -0,0 +1,224 @@
+import locale
+from fastapi import Depends, HTTPException, status
+from config.db_config import SessionLocal
+from dtos.auth_models import UserModel
+from dtos.base_response_model import BaseResponseModel
+from dtos.products_models import (
+    CreateProductModel,
+    ProductListModel,
+    ProductModel,
+    ProductResponseModel,
+    UpdateProductModel,
+)
+from helper.admin_helper import ADMINHelper
+from helper.api_helper import APIHelper
+from helper.token_helper import TokenHelper
+from models.products_table import Product
+from models.categories_table import Category
+from dtos.categories_models import *
+from sqlalchemy.exc import SQLAlchemyError
+import i18n
+import logging
+
+
+class ProductController:
+
+    def read_product(
+        price_min: Optional[float] = None,
+        price_max: Optional[float] = None,
+        rating: Optional[float] = None,
+        discount: Optional[int] = None,
+        category: Optional[int] = None,
+        sort_by: Optional[str] = None,
+        sort_order: Optional[str] = None,
+    ):
+        try:
+            with SessionLocal() as db:
+                query = db.query(Product)
+
+                if price_min is not None:
+                    query = query.filter(Product.price >= price_min)
+                if price_max is not None:
+                    query = query.filter(Product.price <= price_max)
+                if rating is not None:
+                    query = query.filter(Product.rating >= rating)
+                if discount is not None:
+                    query = query.filter(Product.discount >= discount)
+                if category is not None:
+                    query = query.filter(Product.categoryId == category)
+
+                if sort_by:
+                    if sort_order == "descending":
+                        query = query.order_by(getattr(Product, sort_by).desc())
+                    else:
+                        query = query.order_by(getattr(Product, sort_by))
+
+                products = query.all()
+                return [
+                    ProductListModel(
+                        id=product.id,
+                        name=product.name,
+                        stockQuantity=product.stockQuantity,
+                        price=product.price,
+                        categoryId=product.categoryId,
+                        productUrl=product.productUrl,
+                        discount=product.discount,
+                        rating=product.rating,
+                    )
+                    for product in products
+                ]
+        except SQLAlchemyError as e:
+            logging.error(e)
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Database error occurred.",
+            )
+
+    def create_product(product: CreateProductModel, user: UserModel):
+        logger = logging.getLogger(__name__)
+        logger.info(f"Creating product with data: {product}")
+        if ADMINHelper.isAdmin(user):
+            with SessionLocal() as db:
+                new_product = Product(**product.model_dump())
+                try:
+                    logger.info("Adding new product to the database.")
+                    db.add(new_product)
+                    logger.info("Committing the new product to the database.")
+                    db.commit()
+                    db.refresh(new_product)
+                    product_response = ProductResponseModel(
+                        id=new_product.id,
+                        name=new_product.name,
+                        description=new_product.description,
+                        stockQuantity=new_product.stockQuantity,
+                        price=new_product.price,
+                        categoryId=new_product.categoryId,
+                        productUrl=new_product.productUrl,
+                        discount=new_product.discount,
+                        rating=new_product.rating,
+                        isAvailable=new_product.isAvailable,
+                        createdAt=new_product.createdAt,
+                        updatedAt=new_product.updatedAt,
+                    )
+                    return APIHelper.send_success_response(
+                        successMessageKey=i18n.t("translations.PRODUCT_CREATED"),
+                        data=product_response,
+                    )
+
+                except SQLAlchemyError as e:
+                    db.rollback()
+                    logger.error(f"Database error occurred: {str(e)}")
+                    raise HTTPException(
+                        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        detail=f"Database error occurred: {str(e)}",
+                    )
+                except Exception as e:
+                    db.rollback()
+                    logger.error(f"Unexpected error occurred: {str(e)}")
+                    raise HTTPException(
+                        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        detail=f"Unexpected error occurred: {str(e)}",
+                    )
+
+    def read_product_by_id(product_id: int):
+        logger = logging.getLogger(__name__)
+        logger.info(f"Reading product with ID: {product_id}")
+        with SessionLocal() as db:
+            product = db.query(Product).filter(Product.id == product_id).first()
+            if product:
+                product_response = ProductResponseModel(
+                    id=product.id,
+                    name=product.name,
+                    description=product.description,
+                    stockQuantity=product.stockQuantity,
+                    price=product.price,
+                    categoryId=product.categoryId,
+                    productUrl=product.productUrl,
+                    discount=product.discount,
+                    rating=product.rating,
+                    isAvailable=product.isAvailable,
+                    createdAt=product.createdAt,
+                    updatedAt=product.updatedAt,
+                )
+                return product_response
+            else:
+                logger.error(f"Product with ID {product_id} not found.")
+
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail=i18n.t("translations.PRODUCT_NOT_EXIST"),
+                )
+
+    def update_product(product_id: int, product: UpdateProductModel, user: UserModel):
+        logger = logging.getLogger(__name__)
+        logger.info(f"Updating product with ID: {product_id}")
+        if ADMINHelper.isAdmin(user):
+            with SessionLocal() as db:
+                try:
+                    exist_product = (
+                        db.query(Product).filter(Product.id == product_id).first()
+                    )
+                    if exist_product:
+                        for key, value in product.dict().items():
+                            if value is not None:
+                                setattr(exist_product, key, value)
+
+                        db.commit()
+                        db.refresh(exist_product)
+                        return exist_product
+                    else:
+                        logger.error(f"Product with ID {product_id} not found.")
+                        raise HTTPException(
+                            status_code=status.HTTP_404_NOT_FOUND,
+                            detail=i18n.t("translations.PRODUCT_NOT_EXIST"),
+                        )
+                except SQLAlchemyError as e:
+                    logger.error(f"Error updating product: {e}")
+                    db.rollback()
+                    raise HTTPException(
+                        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        detail="Database error occurred.",
+                    )
+                except Exception as e:
+                    logger.error(f"Unexpected error occurred: {e}")
+                    db.rollback()
+                    raise HTTPException(
+                        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        detail="Unexpected error occurred.",
+                    )
+
+    def delete_product(product_id: int, user: UserModel):
+        logger = logging.getLogger(__name__)
+        logger.info(f"Deleting product with ID: {product_id}")
+        if ADMINHelper.isAdmin(user):
+            with SessionLocal() as db:
+                try:
+                    exist_product = (
+                        db.query(Product).filter(Product.id == product_id).first()
+                    )
+                    if exist_product:
+                        db.delete(exist_product)
+                        db.commit()
+                        return APIHelper.send_success_response(
+                            successMessageKey=i18n.t("translations.PRODUCT_DELETED"),
+                        )
+                    else:
+                        logger.error(f"Product with ID {product_id} not found.")
+                        raise HTTPException(
+                            status_code=status.HTTP_404_NOT_FOUND,
+                            detail=i18n.t("translations.PRODUCT_NOT_EXIST"),
+                        )
+                except SQLAlchemyError as e:
+                    logger.error(f"Error deleting product: {e}")
+                    db.rollback()
+                    raise HTTPException(
+                        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        detail="Database error occurred.",
+                    )
+                except Exception as e:
+                    logger.error(f"Unexpected error occurred: {e}")
+                    db.rollback()
+                    raise HTTPException(
+                        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        detail="Unexpected error occurred.",
+                    )

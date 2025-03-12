4046ed03573edabb135a257683a03a2250c4b77b
Bgeekprg <bhaveshpr54@gmail.com>
2025-03-12 15:24:36 +0530

    feat: added order related messages

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,5 @@ from .users_table import User
 from .categories_table import Category
 from .products_table import Product
 from .carts_table import Cart
+from .orders_table import Order
+from .order_details_table import OrderDetail
